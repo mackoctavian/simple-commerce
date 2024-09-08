36ab1dec05db7399eecb14b56d321a7a88609d8f
Mack Octavian <maestrooctavian@gmail.com>
2024-09-09 00:54:58 +0300

    simple demo for zustand

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { create } from "zustand";
+
+const useStore = create<{
+  count: number;
+  inc: () => void;
+  dec: () => void;
+}>((set) => ({
+  count: 0,
+  inc: () => set((state) => ({ count: state.count + 1 })),
+  dec: () => set((state) => ({ count: state.count - 1 })),
+}));
 
 export default function Home() {
+  const store = useStore();
   return (
     <main>
-      <Button>Add</Button>
+      <span>{store.count}</span>
+      <Button onClick={store.inc}>Add</Button>
+      <Button onClick={store.dec}>Minus</Button>
     </main>
   );
 }
